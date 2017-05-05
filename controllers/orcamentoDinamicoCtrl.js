cc8845f21c185bf53950cbfb76e42ce2f929f5fe
Anaximandro Andrade <anaxfive@hotmail.com>
2017-05-05 01:46:56 -0100

    Código para retificar checks funcionando para o básico e impedindo que os requireds forem unchecked, mas precisa de melhor lógica de check e uncheck condicional

--- controllers/orcamentoDinamicoCtrl.js
+++ controllers/orcamentoDinamicoCtrl.js
@@ -3,19 +3,79 @@ APP.controller('OrcamentoDinamico', ['$scope', "$http", OrcamentoController]);
 function OrcamentoController($scope, $http) {
 	var self = this;
 	reset();
-	$scope.tt = "Orçamento Dinâmico";
 
 	$scope.limparSelecoes = _limparSelecoes;
+	$scope.calcTotal = _calcularTotal;
+	$scope.normalize = _rectify;
 
 	// Lógica para Calcular totalAtual
 		// = somatório (item.valor if item.ativo && item.checked)
-	$scope.model.valorTotal = function(){
-            return $scope.model.features.reduce(function(acc, item){
-                return acc + (item.ativo && item.checked ? item.valor : 0);
-            }, 0);
-    };
+	function _calcularTotal(){
+		return $scope.model.features.reduce(function(acc, item){
+            return acc + (item.ativo && item.checked ? item.valor : 0);
+        }, 0);
+	}
 
-	
+	// Resetar ao estado inicial
+		// For item in features if !item.required => item.checked = false
+	function _limparSelecoes(){
+		for (var i=0; i< $scope.model.features.length; i++){
+			if (!$scope.model.features[i].required){
+				$scope.model.features[i].checked = false;
+			}
+		}
+	}
+
+	//Função para verificar se está tudo nos conformes
+	// Todos os required estão checked e todos condicionais
+	//   estão satisfeitos 
+	//@param index indice do item na array
+	//@param it id do item
+	function _rectify(index, it){
+		if (index < 0 ){ //Quando precisa retificar geral
+			for (var i=0; i< $scope.model.features.length; i++){
+				if ($scope.model.features[i].required){
+					$scope.model.features[i].checked = true;
+				}
+			}
+		}else{ // Quando um checkbox for clicado
+			if ($scope.model.features[index].required){
+				$scope.model.features[index].checked = true;
+			}
+
+			// Clik:
+				// Ver required ifs
+					// Loop throug required_ifs
+						// find refered item by id
+						// item.checked = item.checked == required_if.status
+				// Ver unchecked ifs
+				// VEr se é required, e manter sempre on
+			//Checkar subs
+			for (var i=0; i< $scope.model.features.length; i++){
+				if ($scope.model.features[index].checkar.includes($scope.model.features[i].id)){
+
+				}
+
+				if ($scope.model.features[index].descheckar.includes($scope.model.features[i].id)){
+
+				}
+			}
+
+			//Descheckar subs
+		}
+	}
+
+	//Setar tudo do zero!
+	function reset(){
+        $scope.model = {};
+        $scope.model.projeto = {};
+        $scope.model.features = [];
+        $scope.total = 0;
+        $scope.tt = "Orçamento Dinâmico";
+
+
+		_carregarFeatures();
+	}
 
 	function _carregarFeatures (){
 		/* DADOS DO PROJETO VIRÃO DE UMA API
@@ -58,34 +118,10 @@ function OrcamentoController($scope, $http) {
 			}
 		];
 
+		_rectify(-1, -1);
+
 		//Título com base no título do Projeto em questão
 		$scope.tt = "Orçamento de "+ $scope.model.projeto.titulo;
-	}
-
-	// Resetar ao estado inicial
-		// For item in features if !item.required => item.checked = false
-	function _limparSelecoes(){
-		for (var i=0; i< $scope.model.features.length; i++){
-			if (!$scope.model.features[i].required){
-				$scope.model.features[i].checked = false;
-			}
-		}
-	}
-
-	//Função para verificar se está tudo nos conformes
-	// Todos os required estão checked e todos condicionais
-	//   estão satisfeitos 
-	function _rectify(){
-
-	}
-
-
-	//Setar tudo do zero!
-	function reset(){
-        $scope.model = {};
-        $scope.model.projeto = {};
-        $scope.model.features = [];
-		_carregarFeatures();
 	}
 
 }; 
